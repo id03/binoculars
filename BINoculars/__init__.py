6b84614112270e2fd99624147f8abcbb5f9f4baf
Onderwaater <onderwaa@esrf.fr>
2015-03-03 21:15:58 +0100

    including help info on the scripted usage

--- BINoculars/__init__.py
+++ BINoculars/__init__.py
@@ -1,7 +1,27 @@
 import os,sys
 
 # for scripted useage
-def run(args):# args is string as if typed in terminal
+def run(args):    
+    '''Parameters
+        args: string
+            String as if typed in terminal. The string must consist
+            of the location of the configuration file and the command
+            for specifying the jobs that need to be processed. 
+            All additonal configuration file overides can be included
+
+        Returns
+        A BINoculars space
+
+        Examples:
+        >>> space = BINoculars.run('config.txt 10')
+        >>> space
+        Axes (3 dimensions, 2848 points, 33.0 kB) {
+            Axis qx (min=-0.01, max=0.0, res=0.01, count=2)
+            Axis qy (min=-0.04, max=-0.01, res=0.01, count=4)
+            Axis qz (min=0.48, max=4.03, res=0.01, count=356)
+        }
+    '''
+
     import BINoculars.main
     BINoculars.util.register_python_executable(__file__)
     main = BINoculars.main.Main.from_args(args.split(' '))
@@ -12,6 +32,22 @@ def run(args):# args is string as if typed in terminal
         return BINoculars.space.Space.fromfile(filename)
 
 def load(filename):
+    ''' Parameters
+        filename: string
+            Only hdf5 files are acceptable
+
+        Returns
+        A BINoculars space
+
+        Examples:
+        >>> space = BINoculars.load('test.hdf5')
+        >>> space
+        Axes (3 dimensions, 2848 points, 33.0 kB) {
+            Axis qx (min=-0.01, max=0.0, res=0.01, count=2)
+            Axis qy (min=-0.04, max=-0.01, res=0.01, count=4)
+            Axis qz (min=0.48, max=4.03, res=0.01, count=356)
+        }
+    '''
     import BINoculars.space
     if os.path.exists(filename):
         return BINoculars.space.Space.fromfile(filename)
@@ -19,6 +55,25 @@ def load(filename):
         raise IOError("File '{0}' does not exist".format(filename))
 
 def save(filename, space):
+    '''
+        Save a space to file        
+
+        Parameters
+        filename: string
+            filename to which the data is saved.
+        space: BINoculars space
+            the space containing the data that needs to be saved
+
+        Examples:
+        >>> space
+        Axes (3 dimensions, 2848 points, 33.0 kB) {
+            Axis qx (min=-0.01, max=0.0, res=0.01, count=2)
+            Axis qy (min=-0.04, max=-0.01, res=0.01, count=4)
+            Axis qz (min=0.48, max=4.03, res=0.01, count=356)
+        }
+        >>> BINoculars.save('test.hdf5', space)
+    '''
+
     import BINoculars.space
     if isinstance(space, BINoculars.space.Space):
         space.tofile(filename)
@@ -26,6 +81,37 @@ def save(filename, space):
         raise TypeError("'{0!r}' is not a BINoculars space".format(space))
 
 def plot(space, log=True, clipping=0.0, fit=None, norm=None, colorbar=True, labels=True, **plotopts):
+    '''         
+        plots a space with the correct axes. The space can be either one or two dimensinal.
+        
+        Parameters
+        space: BINoculars space
+            the space containing the data that needs to be plotted
+        log: bool
+            axes or colorscale logarithmic
+        clipping: 0 < float < 1
+            cuts a lowest and highst value on the color scale
+        fit: numpy.ndarray
+            same shape and the space. If one dimensional the fit will be overlayed.
+        norm: matplotlib.colors
+            object defining the colorscale
+        colorbar: bool
+            show or not show the colorbar
+        labels: bool
+            show or not show the labels
+        plotopts: keyword arguments
+            keywords that will be accepted by matplotlib.pyplot.plot or matplotlib.pyplot.imshow
+
+        Examples:
+        >>> space
+        Axes (3 dimensions, 2848 points, 33.0 kB) {
+            Axis qx (min=-0.01, max=0.0, res=0.01, count=2)
+            Axis qy (min=-0.04, max=-0.01, res=0.01, count=4)
+            Axis qz (min=0.48, max=4.03, res=0.01, count=356)
+        }
+        >>> BINoculars.plot('test.hdf5')
+    '''
+
     import matplotlib.pyplot as pyplot
     import BINoculars.plot, BINoculars.space
 
@@ -35,7 +121,10 @@ def plot(space, log=True, clipping=0.0, fit=None, norm=None, colorbar=True, labe
         raise TypeError("'{0!r}' is not a BINoculars space".format(space))
 
 def transform(space, labels, resolutions, exprs):
-    ''' Parameters
+    ''' 
+        transformation of the coordinates.
+        
+        Parameters
         space: BINoculars space
         labels: list
             a list of length N with the labels
